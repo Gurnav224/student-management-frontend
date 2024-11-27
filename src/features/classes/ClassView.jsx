9457ac1933495079ecea906eb73567457851d663
gurnav224 <chaudharyg856@gmail.com>
2024-11-27 10:34:31 +0000

    fix styling  in classview

--- src/features/classes/ClassView.jsx
+++ src/features/classes/ClassView.jsx
@@ -1,57 +1,71 @@
 import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux";
 import { fetchStudents, setFilter, setSortBy } from "../students/studentSlice";
 
 const ClassView = () => {
-    const {students} = useSelector((state) => state.students);
-    const dispatch = useDispatch();
+  const { students } = useSelector((state) => state.students);
+  const dispatch = useDispatch();
 
-    useEffect(()=> {
-dispatch(fetchStudents())
-    },[])
+  useEffect(() => {
+    dispatch(fetchStudents());
+  }, []);
 
-    const handleFilterChange = (event) => {
-        dispatch(setFilter(event.target.value))
-    }
+  const handleFilterChange = (event) => {
+    dispatch(setFilter(event.target.value));
+  };
 
-    const handleSortChange = (event) => {
-        dispatch(setSortBy(event.target.value))
-    }
+  const handleSortChange = (event) => {
+    dispatch(setSortBy(event.target.value));
+  };
 
-    
-    return (
+  return (
     <div className="container">
-        <h1 className="my-4">Class View</h1>
-        
-        <div className="row">
-            <div className="col-md-6 my-4">
-            <label className="form-label" htmlFor="filter-by-gender">Filter By Gender</label>
-            <select onChange={handleFilterChange} className="form-select" name="filter-by-gender" id="filter-by-gender">
-                <option value="All">All</option>
-                <option value="male">Boys</option>
-                <option value="female">Girls</option>
-            </select>
-            </div>
-            <div className="col-md-6 my-4">
-                <label className="form-label" htmlFor="sortBy">Sort By</label>
-                <select className="form-select" onChange={handleSortChange} name="sortBy" id="sortBy">
-                    <option value="name">Name</option>
-                    <option value="marks">Marks</option>
-                    <option value="attendance">Attendance</option>
-                </select>
-            </div>
-        </div>
-        <ul>
-            {
-                students.map((student) => (
-                <li  key={student._id}>
-                   <p className="fw-bold">{student.name} - {student.age} - {student.gender} - Marks: {student.marks} - Attendance: - {student.attendance}</p>
-                   </li>
-             ))
-            }
-        </ul>
-    </div>
-  )
-}
+      <h1 className="my-4">Class View</h1>
 
-export default ClassView
+      <div className="row">
+        <div className="col-md-6 my-2">
+          <label className="form-label" htmlFor="filter-by-gender">
+            Filter By Gender
+          </label>
+          <select
+            onChange={handleFilterChange}
+            className="form-select"
+            name="filter-by-gender"
+            id="filter-by-gender"
+          >
+            <option value="All">All</option>
+            <option value="male">Boys</option>
+            <option value="female">Girls</option>
+          </select>
+        </div>
+        <div className="col-md-6 mb-3 ">
+          <label className="form-label" htmlFor="sortBy">
+            Sort By
+          </label>
+          <select
+            className="form-select"
+            onChange={handleSortChange}
+            name="sortBy"
+            id="sortBy"
+          >
+            <option value="name">Name</option>
+            <option value="marks">Marks</option>
+            <option value="attendance">Attendance</option>
+          </select>
+        </div>
+      </div>
+      <ul className="list-group">
+        {students.map((student) => (
+          <li className="list-group-item" key={student._id}>
+            <p className="fw-bold">
+              {student.name} - {student.age} - {student.gender} - Marks:{" "}
+              {student.marks} - Attendance: - {student.attendance}
+            </p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ClassView;
